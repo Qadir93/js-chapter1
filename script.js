985742d804ceb46a7ab666cd33984917112a006e
Qadir93 <abqadirkhan3@gmail.com>
2025-03-25 15:15:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+{
+    alert("Welcome to our website! We hope you have a great experience.");
+}
+
+{
+    alert("Error! Please enter a valid password.");
+}
+
+{
+    alert("Welcome to JS Land...\nHappy Coding!");
+}
+
+console.log("Hello... I can run JS through my web browser console");
